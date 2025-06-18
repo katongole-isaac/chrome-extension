61079fcbeb3205b713f4d03c2777927c5176ad54
Isaac Katongole <katongolelsaac78@gmail.com>
2025-06-18 16:57:35 +0300

    chore:Add a service worker

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         index: path.resolve(__dirname, "index.html"),
         "scripts/content": path.resolve(__dirname, "src/scripts/content.ts"),
+        background: path.resolve(__dirname, "src/service-worker/background.ts"),
       },
       output: {
         entryFileNames: "[name].js",
